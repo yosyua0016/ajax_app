9e034e25f47357dda0b0d6d66fc0176fdcb37d2a
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-13 17:12:54 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,13 @@
 function post (){
-  console.log("イベント発火");
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 };
 
 window.addEventListener('turbo:load', post);
